7319aca19ac783917f54afd9b0fe4e455ee439e7
Gábor Farkas <farkas.gz@gmail.com>
2023-06-23 01:12:54 +0200

    heyooo

--- js/game.js
+++ js/game.js
@@ -1,3 +1,46 @@
 class Game {
     // code to be added
+    constructor() {
+        this.startScreen = document.getElementById('game-intro');
+        this.gameScreen = document.getElementById('game-screen');
+        this.gameEndScreen = document.getElementById('game-end');
+
+        this.height = 600;
+        this.width = 500;
+
+        this.player = new Player(
+            this.gameScreen,
+            210,
+            480,
+            80,
+            150,
+            "images/car.png"
+        );
+        this.obstacles = [];
+
+        this.score = 0;
+        this.lives = 3;
+
+        this.gameIsOver = false;
+    }
+
+    start(){
+        this.gameScreen.style.height = `${this.height}px`;
+        this.gameScreen.style.width = `${this.width}px`;
+        this.startScreen.style.display = 'none';
+        this.gameScreen.style.display = 'block';
+        this.gameloop();
+    }
+
+    gameloop(){
+        if (this.gameIsOver){
+            this.endGame();
+        }
+        this.update();
+        window.requestAnimationFrame(() => this.gameloop());
+    }
+
+    update(){
+        this.player.move();
+    }
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  let game;
 
   startButton.addEventListener("click", function () {
     startGame();
@@ -8,5 +9,66 @@ window.onload = function () {
 
   function startGame() {
     console.log("start game");
+
+    game = new Game();
+    game.start();
+  }
+
+  window.addEventListener("keydown", playerPress);
+
+  function playerPress(event){
+    const key = event.key
+    const possibleKeystrokes = [
+      "ArrowUp",
+      "ArrowDown",
+      "ArrowLeft",
+      "ArrowRight",
+    ];
+
+    if (possibleKeystrokes.includes(key)){
+      event.preventDefault();
+
+      switch (key){
+        case "ArrowUp":
+          game.player.directionY = -1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+          break;
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+      }
+    }
+  }
+
+  window.addEventListener("keyup", playerRelease);
+
+  function playerRelease(event){
+    const key = event.key
+    const possibleKeystrokes = [
+      "ArrowUp",
+      "ArrowDown",
+      "ArrowLeft",
+      "ArrowRight",
+    ];
+
+    if (possibleKeystrokes.includes(key)){
+      event.preventDefault();
+
+      switch (key){
+        case "ArrowUp":
+        case "ArrowDown":
+          game.player.directionY = 0;
+          break;
+        case "ArrowLeft":
+        case "ArrowRight":
+          game.player.directionX = 0;
+          break;
+      }
+    }
   }
 };
